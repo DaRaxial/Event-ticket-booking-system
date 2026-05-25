31cffdbb46c94b3dc083204d5f1279c720d26d5d
DaRaxial <justyaroon@gmail.com>
2026-05-25 14:37:22 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EventSeatManager.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EventSeatManager.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
